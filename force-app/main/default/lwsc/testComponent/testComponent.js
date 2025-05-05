3435072ea52f6d37e2f359470a260ac9e4bd4613
veeranjaneya reddy <140852436+veeranjaneya-reddy@users.noreply.github.com>
2025-05-05 15:54:47 +0530

    Update testComponent.js

--- force-app/main/default/lwsc/testComponent/testComponent.js
+++ force-app/main/default/lwsc/testComponent/testComponent.js
@@ -1,1 +1,7 @@
 
+import { LightningElement } from 'lwc';
+export default class TestComponent extends LightningElement {
+    connectedCallback() {
+        console.log('Test component connected');
+    }
+}
